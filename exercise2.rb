e2e24f9b39a36c799bceb8dc2258cf955871f79c
5-5-C <jmarshall.brock@gmail.com>
2017-01-16 16:44:48 -0500

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#$55 dollar meal with a good tip
+
+puts "a $55 meal would have a 20% tip, this will equate to $#{55*0.20}"
+
+#adding a string and an integer with the + operator.  What happns? Find a way to convert the integer into a string first and use puts to print the result
+
+puts "i like milk" + 4.to_s
+
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+puts "after a weekend of sobriety, i can say that i save #{45628 * 7893} brain cells"
+
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
